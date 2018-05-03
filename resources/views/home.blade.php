218f7191d1046a37922802e4d4d47ebf9630dc7f
jose1914luis <jose1914luis@gmail.com>
2018-05-03 17:24:48 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,20 +8,55 @@
 
 @section('content')
 
-<div style="margin-top: 100px;"> 
-
-<form>
-
-	<div class="input-group mb-3 col-sm-8">
-	  <input type="text" name="empresa" id="empresa" class="form-control form-control-lg" placeholder="Buscar una empresa">
-	  <input type="hidden" name="empresa_id" id="empresa_id" value="">  
-	  <div class="input-group-append">
-	    <button class="btn btn-outline-secondary" type="button"><span class="fa fa-search"></span>&nbsp;</button>
-	  </div>
-	</div>
+<div class="div_img_home">
 	
-</form>
+	<div > 
+
+		<form>
+
+			<div class="input-group mb-3 col-sm-8">
+			  <input type="text" name="empresa" id="empresa" class="form-control form-control-lg" placeholder="Buscar una empresa">
+			  <input type="hidden" name="empresa_id" id="empresa_id" value="">  
+			  <div class="input-group-append">
+			    <button style="background-color: darkgrey" class="btn btn-outline-secondary" type="button"><span class="fa fa-search"></span>&nbsp;</button>
+			  </div>
+			</div>
+			
+		</form>		
+
+	</div>
+
+	<div class="div_sloga">
+
+		<b>Califica tu <span style="color: yellow;">pasión</span>. Que tu opinión cuente!</b>
+	</div>
 
 </div>
+
+<div class="container">
+	<h2>Nuestra Misión</h2>
+	<p class="text-justify">
+	En Ocupasión.com trabajamos para el derecho de cada ciudadano de la República de Colombia por un trabajo con condiciones laborales justas, remuneración justa y trato interpersonal justo.
+	</p>
+
+	<p class="text-justify">
+	Por lo tanto, queremos pedirte francamente que evalúes tu empleador con mayor sinceridad y equidad posible para que buenas empresas tengan la valoración que se merezcan y las malas tengan incentivo a constantemente mejorar.	
+	</p>
+
+	<p class="text-justify">
+	Para que los resultados de las evaluaciones reflejen la realidad queremos pedirte que uses la oportunidad de evaluar a tu empleador de manera anónima y gratuita. Así no solo ayuda a hacer el mercado laboral colombiano más transparente y justo,sino también, aporta un valor agregado para los buscadores actuales y futuros de empleo.	
+	</p>
+
+	<button class="btn btn-outline-warning btn-sm" type="button" onclick="window.location.href='/empresa_evaluar'"><b>EVALUACIÓN ANÓNIMA</b></button>    
+	
+</div>
+<br>
+<br>
+
+
+
+
+
+
 
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--link href="/css/app.css" rel="stylesheet" type="text/css"/-->    
+        <link href="/css/estilos_app.css" rel="stylesheet" type="text/css">    
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -16,47 +16,63 @@
         
     </head>
     <body>
-        @section('sidebar')
 
-          <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
 
-            <a href="/" class="navbar-brand">
-              <b><span  class="text-light"><span class="fa fa-eercast"></span>cu</span><span class="text-warning">Pasion</span></b>                
-            </a>
+          <a href="/" class="navbar-brand">
+            <h1 class="font-italic"><b><span  class="text-light"><span class="fa fa-eercast"></span>cu</span><span class="text-warning">Pasión</span></b></h1>
+          </a>
 
-              <!-- Toggler/collapsibe Button -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+            <!-- Toggler/collapsibe Button -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="/empresa_new">Crear Empresa</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/empresa_list">Lista de Empresas</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Crear Sector</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Crear Categoria</a>
-                </li>
-              </ul>    
-                      
-            </div>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="/empresa_new">Crear Empresa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/empresa_list">Lista de Empresas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Crear Sector</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Crear Categoria</a>
+              </li>
+            </ul>    
+                    
+          </div>
 
-            <form class="form-inline my-2 my-lg-0">
-              <a href="/buscar_empresa" class="text-light mr-sm-2"><span class="fa fa-search"></span> Buscar</a>
-              <button class="btn btn-outline-warning" type="button" onclick="window.location.href='/empresa_evaluar'">Evaluar Empresa</button>                   
-            </form>     
+          <form class="form-inline my-2 my-lg-0">
+             <button type="button" class="btn btn-link btn-sm text-light" onclick="window.location.href='/buscar_empresa'"><b>MI OCUPASIÓN</b></button>
+            <button type="button" class="btn btn-link btn-sm text-light" onclick="window.location.href='/buscar_empresa'"><span class="fa fa-search"></span> <b>BUSCAR</b></button>
+            <button class="btn btn-outline-warning btn-sm" type="button" onclick="window.location.href='/empresa_evaluar'"><b>EVALUACIÓN ANÓNIMA</b></button>                   
+          </form>     
 
-          </nav>  
-        @show
+        </nav>  
 
-        <div class="container">
+        <div>
             @yield('content')
         </div>
+
+        <div class="footer">
+
+          <h3 class="font-italic"><b><span  class="text-light"><span class="fa fa-eercast"></span>cu</span><span class="text-warning">Pasión</span></b></h3>
+          <hr>
+
+          <p>Ocupacion</p>
+          <h1>Description Lists</h1>    
+  <p>The dl element indicates a description list:</p>
+  <dl>
+    <dt>Coffee</dt>
+    <dd>- black hot drink</dd>
+    <dt>Milk</dt>
+    <dd>- white cold drink</dd>
+  </dl>  
+        </div>
+
     </body>        
 </html>
