fd894f1ce24b5ee9795cae95d80efc91841f84a6
jose1914luis <jose1914luis@gmail.com>
2018-08-14 23:04:10 -0400

    cualquier cosa

--- resources/views/empresa_list.blade.php
+++ resources/views/empresa_list.blade.php
@@ -5,11 +5,11 @@
 
 @section('content')
 <h4>Empresas</h4>
-
+<!--href="/empresa/{{$empresa->id}}"-->
 <div class="list-group">
   @foreach ($empresas as $empresa)
 
-    <a class="list-group-item list-group-item-action" href="/empresa/{{$empresa->id}}" >{{$empresa->razon_social}}</a>
+    <a class="list-group-item list-group-item-action"  >{{$empresa->razon_social}}</a>
   @endforeach
 </div>
 
