92f06d8d3cef9b17b7af00c1ef492ef9b13295ad
jose1914luis <jose1914luis@gmail.com>
2018-08-17 17:15:28 -0400

    commit automatico - 2018_08_17__17_15_28

--- app/Evaluacion.php
+++ app/Evaluacion.php
@@ -8,4 +8,5 @@ class Evaluacion extends Model
 {
     protected $table = 'evaluaciones';
     protected $fillable = array('empresa_id', 'evalua', 'trabajo_tiempo', 'posicion', 'departamento', 'titulo', 'comentarios', 'mejoras', 'like', 'no_like', 'recomienda', 'beneficios', 'email', 'salario', 'ofrecer', 'oferta', 'confir_code');
+    protected $guarded = ['confirmed'];
 }
