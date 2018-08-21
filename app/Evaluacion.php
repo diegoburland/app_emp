ce85730d3f25ba0284caf0b553c79514a417628f
jose1914luis <jose1914luis@gmail.com>
2018-08-21 11:48:49 -0400

    commit automatico - 2018_08_21__11_48_49

--- app/Evaluacion.php
+++ app/Evaluacion.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Evaluacion extends Model
 {
     protected $table = 'evaluaciones';
-    protected $fillable = array('confirmed', 'empresa_id', 'evalua', 'trabajo_tiempo', 'posicion', 'departamento', 'titulo', 'comentarios', 'mejoras', 'like', 'no_like', 'recomienda', 'beneficios', 'email', 'salario', 'ofrecer', 'oferta', 'confir_code');
+    protected $fillable = array('contenido', 'publicada', 'ip', 'confirmed', 'empresa_id', 'evalua', 'trabajo_tiempo', 'posicion', 'departamento', 'titulo', 'comentarios', 'mejoras', 'like', 'no_like', 'recomienda', 'beneficios', 'email', 'salario', 'ofrecer', 'oferta', 'confir_code');
   
     /*public function setConfirmedAttribute($confirmed)
     {
--- database/migrations/2018_03_31_153159_create_evaluaciones_table.php
+++ database/migrations/2018_03_31_153159_create_evaluaciones_table.php
@@ -31,9 +31,12 @@ class CreateEvaluacionesTable extends Migration
             $table->boolean('recomienda')->nullable();
             $table->mediumText('beneficios')->nullable();
 
-            $table->string('confir_code', 250)->nullable();
-
-            $table->boolean('confirmed')->default(false);
+            $table->string('confir_code', 250)->nullable();            
+            
+            $table->string('confirmed', 50)->default('NO');
+            $table->string('contenido', 50);
+            $table->string('publicada', 50)->default('NO');
+            $table->string('ip', 100)->nullable();
 
             $table->timestamps();
 
