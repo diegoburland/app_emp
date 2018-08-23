4e2e67772fea67d03021fd6a040458ddb0fd3454
jose1914luis <jose1914luis@gmail.com>
2018-08-23 17:44:04 -0400

    commit automatico - 2018_08_23__17_44_04

--- app/Eval_bene.php
+++ app/Eval_bene.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Eval_bene extends Model
 {
-<<<<<<< HEAD
-=======
-     protected $table = 'eval_benes'; 
->>>>>>> 5170f837a3d9143e09b731e4867def2f9d3b264d
+     protected $table = 'eval_benes';  
      protected $fillable = array('evaluacion_id', 'bene_id');
 }
