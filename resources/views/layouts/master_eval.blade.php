65333d8cb185bd316681e997896b7c5753ba9a81
jose1914luis <jose1914luis@gmail.com>
2018-09-20 18:18:46 -0400

    commit automatico - 2018_09_20__18_18_46

--- resources/views/layouts/master_eval.blade.php
+++ resources/views/layouts/master_eval.blade.php
@@ -2,7 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/css/estilos_app.css" rel="stylesheet" type="text/css">    
+        <link href="/css/estilos_app.css?v={{ time() }}" rel="stylesheet" type="text/css">    
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
