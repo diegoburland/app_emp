cf0cb57f5f83f294fcecee526f4c88da7b5314e3
jose1914luis <jose1914luis@gmail.com>
2018-09-26 14:47:54 -0400

    commit automatico - 2018_09_26__14_47_54

--- app/Evaluacion.php
+++ app/Evaluacion.php
@@ -8,15 +8,10 @@ use DB;
 class Evaluacion extends Model
 {
     protected $table = 'evaluaciones';
-<<<<<<< HEAD
     protected $fillable = array('porque', 'motivo', 'contenido', 'publicada', 'ip', 'confirmed', 'empresa_id', 'evalua', 'trabajo_tiempo', 'posicion', 'departamento', 'titulo', 'comentarios', 'mejoras', 'like', 'no_like', 'recomienda', 'beneficios', 'email', 'salario', 'ofrecer', 'oferta', 'confir_code', 'ies', 'ciudad_eval_id', 'id_padre');
   
     /*public function setConfirmedAttribute($confirmed)
     {
         $this->attributes['confirmed'] = $confirmed;
     }*/
-=======
-    protected $fillable = array('contenido', 'publicada', 'ip', 'confirmed', 'empresa_id', 'evalua', 'trabajo_tiempo', 'posicion', 'departamento', 'titulo', 'comentarios', 'mejoras', 'like', 'no_like', 'recomienda', 'beneficios', 'email', 'salario', 'ofrecer', 'oferta', 'confir_code', 'id_padre');
- 
->>>>>>> ca4bbfce86e015424ae11dd9b420be1d89c17dab
 }
