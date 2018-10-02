0c1e621f41a30dce6eb9187320a62396fc549ec9
Luis Eduardo Nieto <luiseduardo.nieto92@gmail.com>
2018-10-01 22:47:29 -0500

    Cambios que faltaban

--- app/Eval_bene.php
+++ app/Eval_bene.php
@@ -3,9 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Eval_bene extends Model
 {
      protected $table = 'eval_benes';  
      protected $fillable = array('evaluacion_id', 'bene_id');
+
+    public function getBeneByEvaluation($idEval){
+
+        $results = array();
+
+        $queries = DB::table('eval_benes')
+            ->select('eval_benes.id as id', 'eval_benes.bene_id')
+            ->where('eval_benes.evaluacion_id', '=', $idEval)
+            ->get();
+
+        return $queries;
+    }
 }
