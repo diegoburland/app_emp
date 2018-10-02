cb2705011278adf3d6b01d663f98471daaf5b30c
Luis Eduardo Nieto <luiseduardo.nieto92@gmail.com>
2018-10-01 22:32:59 -0500

    Cambios faltantes en el listado de evaluacion

--- public/js/empresa/evaluacionList.js
+++ public/js/empresa/evaluacionList.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     $('#listEvaluacion').DataTable({
-    	"processing": true,
-    	"searching": false,
-	   "lengthMenu": [[50,70,80,100], [50,70,80,100]],
-    	"language": {
-    		"url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
-		    "paginate": {
-		    "first": "Primero",
+        "processing": true,
+        "searching": true,
+       "lengthMenu": [[50,70,80,100], [50,70,80,100]],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
+            "paginate": {
+            "first": "Primero",
             "last": "Ultimo",
             "next": "Siguiente",
             "previous": "Anterior"
-		    }
-	  	},
-	  	"columnDefs": [
+            }
+        },
+        "columnDefs": [
         {"className": "dt-center", "targets": "_all"}
       ]
-	  	
+        
     });
 
     var table = $('#listEvaluacion').DataTable();
