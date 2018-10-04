bfbe19222ac2db7688591050af466213d17fdaf1
IEUser <IEUser@10.0.2.4>
2018-10-04 11:36:55 -0700

    modificaciones visuales

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -56,9 +56,9 @@
           </div>
 
           <form class="form-inline my-2 my-lg-0">
-             <button type="button" class="btn btn-link btn-sm text-light" onclick="window.location.href='/login'"><b>MI OCUPASIÓN</b></button>
-            <button type="button" class="btn btn-link btn-sm text-light" onclick="window.location.href='/buscar_empresa'"><span class="fa fa-search"></span> <b>BUSCAR</b></button>
-            <button class="btn btn-warning btn-sm" type="button" onclick="window.location.href='/empresa_evaluar'"><b>EVALUACIÓN ANÓNIMA</b></button>                   
+             <button type="button" class="btn btn-link text-warning" onclick="window.location.href='/login'"><b>MI CUENTA</b></button>
+            <!--button type="button" class="btn btn-link btn-sm text-light" onclick="window.location.href='/buscar_empresa'"><span class="fa fa-search"></span> <b>BUSCAR</b></button-->
+            <button class="btn btn-warning " type="button" onclick="window.location.href='/empresa_evaluar'"><b>EVALUACIÓN ANÓNIMA</b></button>                   
           </form>     
 
         </nav>  
