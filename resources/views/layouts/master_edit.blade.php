2dcad4ea44d8419464bec46dc319dca2ae0334b7
IEUser <IEUser@10.0.2.4>
2018-10-17 07:14:39 -0700

    remove scripts

--- resources/views/layouts/master_edit.blade.php
+++ resources/views/layouts/master_edit.blade.php
@@ -30,24 +30,7 @@
             })(document);
             smartlook('init', 'f2519ee6854e705a7666bcb2d045b22e207ed679');
         </script>
-        
-        <!-- Facebook Pixel Code -->
-        <script>
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '337969703625194');
-          fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=337969703625194&ev=PageView&noscript=1"
-        /></noscript>
-        <!-- End Facebook Pixel Code -->
+               
 
         @section('head')
 
