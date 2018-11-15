879e0d1c240971548bceec649adad2bd3a9f79f4
IEUser <IEUser@10.0.2.15>
2018-11-15 08:06:04 -0800

    calculator for new functionality

--- app/Classes/Calculator.php
+++ app/Classes/Calculator.php
@@ -3,49 +3,62 @@
 namespace App\Classes;
 
 use DB;
+use App\Evaluacion;
 
-class Calculator
-{
-    public function average_eval(){
-        
-    }
-    
-    private function average_vw(){
+class Calculator {
+
+    private static $AVG_1 = 4.4;
+    private static $AVG_2 = 3.4;
+    private static $AVG_3 = 2.4;
+
+    public function average_eval($id) {
+
         $avg = DB::table('evaluaciones')
-            ->join('eval_items', 'eval_items.evaluacion_id', '=', 'evaluaciones.id')
-            ->select(DB::raw('avg(eval_items.puntaje) as promedio'))
-            ->where('eval_items.puntaje', '>', 0)->first();
-        return $avg->promedio;
+                        ->join('eval_items', 'eval_items.evaluacion_id', '=', 'evaluaciones.id')
+                        ->select(DB::raw('avg(eval_items.puntaje) as avg_eval'))
+                        ->where([['eval_items.puntaje', '>', 0],
+                                ['id', '=', $id]])->first();
+        return $avg->avg_eval;
     }
-    
-    public function macth_result($avg_eval){
-        
+
+    private function average_vw() {
+        $avg = DB::table('evaluaciones')
+                        ->join('eval_items', 'eval_items.evaluacion_id', '=', 'evaluaciones.id')
+                        ->select(DB::raw('avg(eval_items.puntaje) as avg_vw'))
+                        ->where('eval_items.puntaje', '>', 0)->first();
+        return $avg->avg_vw;
+    }
+
+    public function macth_result($id_eval) {
+
         $file = file_get_contents(__DIR__ . "\output.json");
         $json = json_decode($file, true);
         $avg_vw = $this->average_vw();
-        
-        $string="";
-        switch ($avg_eval) {
-            case $avg_eval>$avg_vw:
+        $evaluation = Evaluacion::find($id_eval);
+        $avg_eval = $this->average_eval($id_eval);
 
-                $string = $avg_eval .">" . $avg_vw;
-                break;
-
-            default:
-                break;
+        //get first resul 
+        $string = "";
+        if ($avg_eval < $avg_vw) {
+            $string = str_replace("$1", $avg_eval, $json["case-1"]);
+            $string = str_replace("$2", $avg_vw, $string);
+        } else if ($avg_eval >= $avg_vw) {
+            $string = str_replace("$1", $avg_eval, $json["case-2"]);
+            $string = str_replace("$2", $avg_vw, $string);
         }
-        
-        
-        //$string = $json["case-1"];
-        /*foreach($json as $i => $v)
-        {
-            $string = $string. $i;
-            $string = $string. $v;
-        }*/
-         
+
+        //get interpretations
+        if ($avg_eval >= self::$AVG_1) {
+            $string = $string . $json["case-3"];
+        } else if (self::$AVG_1 > $avg_eval && $avg_eval >= self::$AVG_2) {
+            $string = $string . $json["case-4"];
+        } else if (self::$AVG_2 > $avg_eval && $avg_eval >= self::$AVG_3) {
+            $string = $string . $json["case-5"];
+        }if (self::$AVG_3 > $avg_eval) {
+            $string = $string . $json["case-6"];
+        }
+
         return $string;
     }
-    
-    
-}
 
+}
