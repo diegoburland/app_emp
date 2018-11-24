90e14aa65190ac95b433fbd0216dac5d06f684e4
jose1914luis <jose1914luis@gmail.com>
2018-11-24 11:37:00 -0500

    new delivery 2

--- app/Classes/Calculator.php
+++ app/Classes/Calculator.php
@@ -63,37 +63,40 @@ class Calculator {
 
         $file = file_get_contents(__DIR__ . "/output.json");
         $json = json_decode($file, true);
-        $avg_vw = $this->average_vw();
+        $avg_vw = round($this->average_vw(), 2);
         $evaluation = Evaluacion::find($id_eval);
-        $avg_eval = $this->average_eval($id_eval);
+        $avg_eval = round($this->average_eval($id_eval),2);
 
         
-        $cases = [];
+      
         if($evaluation->evalua == "Trabajo Actual"){
-            $cases = [1,2,3,4,5,6,7,8,9,10,11];
+            
+            $json = $json["current_job"];
         }else if($evaluation->evalua == "Trabajo Pasado"){
-            $cases = [12,13,14,15,16,17,18,19,20,21,22];
+             $json = $json["past_job"];
+        }else if($evaluation->evalua == "Práctica"){
+             $json = $json["pratice_job"];
         }
         
         
         $string = "";
         if ($avg_eval < $avg_vw) {
-            $string = str_replace("$1", $avg_eval, $json["case-" . $cases[0]]);
+            $string = str_replace("$1", $avg_eval, $json[ 1]);
             $string = str_replace("$2", $avg_vw, $string);
         } else if ($avg_eval >= $avg_vw) {
-            $string = str_replace("$1", $avg_eval, $json["case-" . $cases[1]]);
+            $string = str_replace("$1", $avg_eval, $json[ 2]);
             $string = str_replace("$2", $avg_vw, $string);
         }
 
         //get interpretations
         if ($avg_eval >= self::$AVG_1) {
-            $string = $string . $json["case-" . $cases[2]];
+            $string = $string . $json[ 3];
         } else if (self::$AVG_1 > $avg_eval && $avg_eval >= self::$AVG_2) {
-            $string = $string . $json["case-" . $cases[3]];
+            $string = $string . $json[ 4];
         } else if (self::$AVG_2 > $avg_eval && $avg_eval >= self::$AVG_3) {
-            $string = $string . $json["case-". $cases[4]];
+            $string = $string . $json[ 5];
         }if (self::$AVG_3 > $avg_eval) {
-            $string = $string . $json["case-". $cases[5]];
+            $string = $string . $json[ 6];
         }
         
         $dims = $this->total_dim_less_two($id_eval);
@@ -105,25 +108,30 @@ class Calculator {
 
                 $str_tem = $str_tem . $dim->nombre . ", ";
             }
-            $string = $string . str_replace("$1", $str_tem, $json["case-". $cases[6]]);
+            $string = $string . str_replace("$1", $str_tem, $json[ 7]);
         }
         
         $total_bene = $this->total_bene_eval($id_eval);
-        $avg_bene = $this->average_bene_vw();
+        $avg_bene = round($this->average_bene_vw(),2);
         
-        
+        //benefy
         if($total_bene < $avg_bene){
-            $string = $string . str_replace("$1", $avg_bene, $json["case-". $cases[7]]);
+            $string = $string . str_replace("$1", $avg_bene, $json[ 8]);
         }else{
-            $string = $string . str_replace("$1", $avg_bene, $json["case-". $cases[8]]);
+            $string = $string . str_replace("$1", $avg_bene, $json[ 9]);
         }
         
+        //worked time
         if($evaluation->trabajo_tiempo > 0){
             if($evaluation->trabajo_tiempo > self::$HOUR_1){
 
-                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-". $cases[9]]);
+                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json[ 10]);
+                $more = $evaluation->trabajo_tiempo - self::$HOUR_1;
+                $string = str_replace("$2", $more, $string);
             }else{
-                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-". $cases[10]]);
+                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json[ 11]);
+                $less = self::$HOUR_1 - $evaluation->trabajo_tiempo;
+                $string = str_replace("$2", $less, $string);
             }    
         }
         
--- app/Http/Controllers/Retro_controller.php
+++ app/Http/Controllers/Retro_controller.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Classes\Calculator;
 
-class Prueba_controller extends Controller
+class Retro_controller extends Controller
 {
     public function show($id)
     {
         $calculator = new Calculator();
-        return $calculator->macth_result(7);
+        return $calculator->macth_result($id);
         //return $calculator->average_vw();
     }
 }
