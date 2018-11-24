f1c4c685d404abc57f182dce8b92c26770119a75
jose1914luis <jose1914luis@gmail.com>
2018-11-24 09:53:22 -0500

    new delivery

--- app/Classes/Calculator.php
+++ app/Classes/Calculator.php
@@ -61,31 +61,39 @@ class Calculator {
 
     public function macth_result($id_eval) {
 
-        $file = file_get_contents(__DIR__ . "\output.json");
+        $file = file_get_contents(__DIR__ . "/output.json");
         $json = json_decode($file, true);
         $avg_vw = $this->average_vw();
         $evaluation = Evaluacion::find($id_eval);
         $avg_eval = $this->average_eval($id_eval);
 
-        //get first resul 
+        
+        $cases = [];
+        if($evaluation->evalua == "Trabajo Actual"){
+            $cases = [1,2,3,4,5,6,7,8,9,10,11];
+        }else if($evaluation->evalua == "Trabajo Pasado"){
+            $cases = [12,13,14,15,16,17,18,19,20,21,22];
+        }
+        
+        
         $string = "";
         if ($avg_eval < $avg_vw) {
-            $string = str_replace("$1", $avg_eval, $json["case-1"]);
+            $string = str_replace("$1", $avg_eval, $json["case-" . $cases[0]]);
             $string = str_replace("$2", $avg_vw, $string);
         } else if ($avg_eval >= $avg_vw) {
-            $string = str_replace("$1", $avg_eval, $json["case-2"]);
+            $string = str_replace("$1", $avg_eval, $json["case-" . $cases[1]]);
             $string = str_replace("$2", $avg_vw, $string);
         }
 
         //get interpretations
         if ($avg_eval >= self::$AVG_1) {
-            $string = $string . $json["case-3"];
+            $string = $string . $json["case-" . $cases[2]];
         } else if (self::$AVG_1 > $avg_eval && $avg_eval >= self::$AVG_2) {
-            $string = $string . $json["case-4"];
+            $string = $string . $json["case-" . $cases[3]];
         } else if (self::$AVG_2 > $avg_eval && $avg_eval >= self::$AVG_3) {
-            $string = $string . $json["case-5"];
+            $string = $string . $json["case-". $cases[4]];
         }if (self::$AVG_3 > $avg_eval) {
-            $string = $string . $json["case-6"];
+            $string = $string . $json["case-". $cases[5]];
         }
         
         $dims = $this->total_dim_less_two($id_eval);
@@ -97,7 +105,7 @@ class Calculator {
 
                 $str_tem = $str_tem . $dim->nombre . ", ";
             }
-            $string = $string . str_replace("$1", $str_tem, $json["case-7"]);
+            $string = $string . str_replace("$1", $str_tem, $json["case-". $cases[6]]);
         }
         
         $total_bene = $this->total_bene_eval($id_eval);
@@ -105,17 +113,17 @@ class Calculator {
         
         
         if($total_bene < $avg_bene){
-            $string = $string . str_replace("$1", $avg_bene, $json["case-8"]);
+            $string = $string . str_replace("$1", $avg_bene, $json["case-". $cases[7]]);
         }else{
-            $string = $string . str_replace("$1", $avg_bene, $json["case-9"]);
+            $string = $string . str_replace("$1", $avg_bene, $json["case-". $cases[8]]);
         }
         
         if($evaluation->trabajo_tiempo > 0){
             if($evaluation->trabajo_tiempo > self::$HOUR_1){
 
-                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-10"]);
+                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-". $cases[9]]);
             }else{
-                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-11"]);
+                $string = $string . str_replace("$1", $evaluation->trabajo_tiempo, $json["case-". $cases[10]]);
             }    
         }
         
