8a906d9e698b6045fc2e9ee68012c2b6c8274d1c
IEUser <IEUser@10.0.2.15>
2018-11-28 07:17:01 -0800

    email and new styles

--- app/Http/Controllers/Retro_controller.php
+++ app/Http/Controllers/Retro_controller.php
@@ -8,11 +8,9 @@ class Retro_controller extends Controller
 {
     public function show($id)
     {
-        //$calculator = new Calculator();
-        //return $calculator->macth_result($id);
+        $calculator = new Calculator();
+        return $calculator->macth_result($id);
         //return $calculator->average_vw();
-        
-        return view('emails.output');
     }
 }
 
