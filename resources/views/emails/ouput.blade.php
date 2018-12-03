98abea5ee3c808084c6db7079309c06402435997
jose1914luis <jose1914luis@gmail.com>
2018-12-03 14:24:34 -0500

    new delivery 5

--- resources/views/emails/ouput.blade.php
+++ resources/views/emails/ouput.blade.php
@@ -550,6 +550,8 @@ a[x-apple-data-detectors=true] {
         </div>
       </div>
     </div>
+                    
+                     @if ($dimen_detail != null)
     <div style="background-image:url('https://d1oco4z2z1fhwp.cloudfront.net/templates/default/130/funky-lines.png');background-position:top center;background-repeat:repeat;;background-color:#C9C9C9">
       <div style="Margin: 0 auto;min-width: 320px;max-width: 700px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;" class="block-grid ">
         <div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
@@ -575,7 +577,7 @@ a[x-apple-data-detectors=true] {
         </div>
       </div>
     </div>
-                     @if ($dimen_detail != null)
+                    
     <div style="background-image:url('https://d1oco4z2z1fhwp.cloudfront.net/templates/default/130/funky-lines.png');background-position:top center;background-repeat:repeat;;background-color:#C9C9C9">
       <div style="Margin: 0 auto;min-width: 320px;max-width: 700px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;" class="block-grid ">
         <div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
