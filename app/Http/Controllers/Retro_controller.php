18f6aceb0d1814b56cd1455b01869391d51363c7
jose1914luis <jose1914luis@gmail.com>
2018-12-03 14:10:33 -0500

    new delivery 5

--- app/Http/Controllers/Retro_controller.php
+++ app/Http/Controllers/Retro_controller.php
@@ -9,7 +9,7 @@ class Retro_controller extends Controller
     public function show($id)
     {
         $calculator = new Calculator();
-        //$result =  $calculator->macth_result($id);
+        $result =  $calculator->macth_result($id);
         //return $calculator->macth_result($id);
         return view('emails.ouput', $result);
     }
