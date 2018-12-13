1b7f2fa9a27d6ab0a5b1dcd74ad0b92dea1db2c8
jose1914luis <jose1914luis@gmail.com>
2018-12-13 17:22:48 -0500

    new field

---
+++ database/migrations/2018_12_05_040813_add_columns_company.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsCompany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('empresas', function (Blueprint $table) {
+
+            $table->string('clasificacion', 255)->nullable();                       
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('empresas', function (Blueprint $table) {
+        });
+    }
+}
